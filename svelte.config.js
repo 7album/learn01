08ddfe555f47b5d8088c2ee24fa559c1e0dbd71b
7album <sevenalbum@gmail.com>
2026-07-31 03:03:01 +0800

    chore: disable svelte-preprocess temporarily for Svelte 5 build

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
+// using no preprocess for now; update svelte-preprocess to a Svelte 5 compatible version if you need preprocessing
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: [],
   kit: {
     adapter: adapter(),
     alias: {
